8d7d3902fbcbbd44910796779fffd6e8e90c5487
DK1596 <dinas_1996@mail.ru>
2021-05-24 10:17:03 +0600

    was added @value, @scope, @postConstruct and @preDestroy with help annotation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.inversion_of_control.music.ClassicalMusic;
 import org.example.inversion_of_control.music.MusicPlayer;
 import org.example.inversion_of_control.music.enum_music.EnumMusic;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -24,6 +25,11 @@ public class Main {
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic(EnumMusic.ClassicalMusic);
         musicPlayer.playMusic(EnumMusic.JazzMusic);
+
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
+        ClassicalMusic classicalMusic1 = context.getBean("classicalMusic", ClassicalMusic.class);
 
 //        MP3Player mp3Player = context.getBean("mp3", MP3Player.class);
 //        System.out.println(mp3Player.toString());
